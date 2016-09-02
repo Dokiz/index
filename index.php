6fe2a475ce4ed308210804cc89334854804eea36
Dokiz_228@mail.ru <deoptgqqr12>
2016-09-02 17:40:37 +0300

    start

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<html>
+    <head>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    </head>
+<body>
+<meta charset="utf-8"
+    <form action="s_3_2.php">
+	<form action="main.php">
+        <p>name<br><input type="text" name="username"/></br></p>
+        <br><input type="submit" name="enter" value="Регистрацая"/></br> 
+    </form>
+<?php
+$name=5;
+$fam=3;
+$a=$name+$fam;
+echo $a;
+?>
+</body>
+</html>
